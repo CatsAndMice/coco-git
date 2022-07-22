94e21d819c1b723a6db44f8fdc9c4c99259abe4d
CatsAndMice <lihai_js@163.com>
2022-07-22 20:26:29 +0800

    chore: 移动文件至合适的文件夹中

--- bin/cli.js
+++ bin/cli.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 const { program } = require('commander')
 const process = require('process')
-const pkg = require('./package.json')
-const handoffUser = require('./handoffUser')
-const isUpdateVersion = require('./isUpdateVersion')
+const pkg = require('../package.json')
+const handoffUser = require('../src/handoffUser')
+const isUpdateVersion = require('../src/isUpdateVersion')
 program.version(pkg.version).parse(process.argv);
 program.command('tag')
   .description('自动创建git tag并推送远程')
